c402011c80ade365fedfa2f6da8c70cbb318a712
chris way <chris@darksquare.net>
2012-05-28 21:08:05 +0100

    Added Error handling

---
+++ src/ClouDNSIPUpdater.Console/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using ClouDNSIPUpdater.Library;
+
+namespace ClouDNSIPUpdater.Console
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var updater = new DynamicUrlUpdater();
+
+            updater.Update();
+        }
+    }
+}
--- src/ClouDNSIPUpdater.Library/DynamicUrlUpdater.cs
+++ src/ClouDNSIPUpdater.Library/DynamicUrlUpdater.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections.Generic;
 using System.Net;
 using System.Threading.Tasks;
 using ClouDNSIPUpdater.Library.Config;
@@ -15,26 +16,42 @@ namespace ClouDNSIPUpdater.Library
 
         public void Update()
         {
+            var handlerTasks = new List<Task>();
+
+
             foreach (ClouDNSInstanceElement instance in ClouDNSConfig.GetAllInstances())
             {
                 ClouDNSInstanceElement instanceCopy = instance;
 
-                Task<Tuple<string, ClouDNSInstanceElement>>.Factory.StartNew(i =>
-                                                                             {
-                                                                                 var webClient = new WebClient();
-                                                                                 var configSetting = (ClouDNSInstanceElement) i;
+                var updaterTask = Task<Tuple<string, ClouDNSInstanceElement>>.Factory.StartNew(i =>
+                                                                                               {
+                                                                                                   var webClient = new WebClient();
+                                                                                                   var configSetting = (ClouDNSInstanceElement) i;
 
-                                                                                 return new Tuple<string, ClouDNSInstanceElement>(
-                                                                                     webClient.DownloadString(configSetting.DynamicUrl), configSetting);
-                                                                             }, instanceCopy)
-                    .ContinueWith(task =>
-                                  {
-                                      if (ResponseParser.CheckResponse(task.Result.Item1))
-                                          Log(string.Format("Success: {0}", task.Result.Item2.Name));
-                                      else
-                                          Log(string.Format("Failure: {0}", task.Result.Item2.Name));
-                                  });
+                                                                                                   string result = webClient.DownloadString(configSetting.DynamicUrl);
+
+                                                                                                   return new Tuple<string, ClouDNSInstanceElement>(result, configSetting);
+                                                                                               }, instanceCopy);
+                updaterTask.ContinueWith(task =>
+                                         {
+                                             if (ResponseParser.CheckResponse(task.Result.Item1))
+                                                 Log(string.Format("Success: {0}", task.Result.Item2.Name));
+                                             else
+                                                 Log(string.Format("Failure: {0}", task.Result.Item2.Name));
+                                         });
+
+                var handlerTask = updaterTask.ContinueWith(task =>
+                                                           {
+                                                               foreach (var e in task.Exception.Flatten().InnerExceptions)
+                                                                   Log("Observed exception: " + e.Message);
+                                                           },
+                                                           TaskContinuationOptions.OnlyOnFaulted);
+
+
+                handlerTasks.Add(handlerTask);
             }
+
+            Task.WaitAll(handlerTasks.ToArray());
         }
 
         public void Log(string message)
